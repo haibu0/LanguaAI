477ed194facb14b80b35f255b6ec8e6933beb82f
Baller <35856202+haibu0@users.noreply.github.com>
2025-12-03 16:04:58 -0600

    updated frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,7 @@
-console.log("LanguaAI frontend loaded");
+document.getElementById("startBtn").addEventListener("click", () => {
+    console.log("Start recording...");
+});
+
+document.getElementById("stopBtn").addEventListener("click", () => {
+    console.log("Stop recording...");
+});
